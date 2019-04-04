b062fedcdc86886701b8a6726fa526d42a97e3a4
Margaret O'Neill <margaret@oneillfish.com>
2019-04-04 19:48:06 -0400

    adds structure for add and delete store and add and delete item

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,20 @@ const express = require('express');
 
 const mainRouter =  express.Router();
 
-const loadMainPage = require('../controllers/main');
+const { loadMainPage,
+    deleteAStore,
+    deleteAnItem,
+    addStore,
+    addItem} = require('../controllers/main');
 
 mainRouter.post('/',loadMainPage);
 
+mainRouter.post('./delete/store/:id',deleteAStore);
 
+mainRouter.post('/delete/item/:id', deleteAnItem);
+
+mainRouter.post('/add/store', addStore);
+
+mainRouter.post('/add/item', addItem)
 
 module.exports = mainRouter;
