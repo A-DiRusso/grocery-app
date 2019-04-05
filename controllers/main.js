071ebd6f199183497ac472e6883428bad6557666
Margaret O'Neill <margaret@oneillfish.com>
2019-04-04 20:18:58 -0400

    created basic model functions for delete and add

--- controllers/main.js
+++ controllers/main.js
@@ -18,17 +18,35 @@ async function deleteAStore (req, res) {
     //the id will come on the req.params.id
 
     //to delete a store, you need to delete all items for that store
+    
     //then delete the user-store references
     //then delete the store record...
 
     //then re-render the main page with the new list of stores
 
     //re pull the stores based on req.sessions.id -- this is the user id.
-    
+
     //and resave the stores in session
     req.session.stores = userStores;
 
     res.render('main',{locals:{user:req.session.user,storeName:null,stores:userStores,items:[{item:"create New Item"}]}});
+
+    const userStores = await theUser.stores;  //get a list of stores
+
+    // console.log(userStores);
+    // console.log(userStores[0].storeName);
+
+    //store the store array in session
+    req.session.stores = userStores;
+    req.session.user=theUser.firstName;
+    req.session.userID = theUser.id;
+    req.session.save( () => {
+        res.render('main',{locals:{user:theUser.firstName,storeName:null,stores:userStores,items:[{item:"create New Item"}]}});
+
+    })
+
+
+
 
 }
 mainRouter.post('/add/item', addItem)
--- models/items.js
+++ models/items.js
@@ -50,10 +50,23 @@ class Item {
                     password = '${this.password}'
                      where id = ${this.id}`);
     }
+    /* this shouldn't be here
     static insertUser (firstName, lastName, email, password) {
         return db.result(`insert into users
         (first_name, last_name, email, password)
         values  ($1, $2, $3, $4)`, [firstName, lastName, email, password])
+    }*/
+
+    static deleteByStoreID(id) {
+        return db.result(`DELETE from items WHERE store_id = $1`,[id])
+    }
+
+    static addItem(name,quantity,comments,storeId) {
+        return db.result(`INSERT into items (store_id, item, quantity,comments) values ($1, $2, $3, $4)`, [storeId, name, quantity,comments]);
+        //then they need to refresh the page
+    }
+    static deleteItem (id) {
+        return db.result(`DELETE from items WHERE id=$1`,[id]);
     }
 
 }
